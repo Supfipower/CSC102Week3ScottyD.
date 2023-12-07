a049743b6ef54126207fdc8b99f86cfe3c7fb636
Supfipower <151487654+Supfipower@users.noreply.github.com>
2023-12-06 19:24:01 -0500

    Add files via upload

---
+++ CSC102Game.js
@@ -0,0 +1,14 @@
+
+let x;
+let y;
+console.log(x);
+{
+Document.getElementById("rollButton").onclick = Function()
+
+    x = Math.floor(Math.random() * 6) + 1;
+    y = Math.floor(Math.random() * 6) + 1;
+
+    document.getElementById("xlabel").innerHTML = x;
+    document.getElementById("ylabel").innerHTML = y;
+
+}
